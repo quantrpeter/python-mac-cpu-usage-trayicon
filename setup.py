64553028f2cbd0dc55837e9b718ac6439b638acb
Peter <peter@quantr.hk>
2025-11-01 10:09:51 +0800

    dev

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 APP = ['cpu_tray.py']
 OPTIONS = {
     'iconfile': 'blue.png',
-    'packages': ['psutil', 'rumps']
+    'packages': ['psutil', 'rumps'],
+    'excludes': ['pkg_resources', 'setuptools', 'tkinter', 'matplotlib', 'numpy', 'scipy'],
+    'plist': {
+        'LSUIElement': True,
+    }
 }
 
 setup(
